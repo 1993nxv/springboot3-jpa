15500024df8e128344530772de03b3692f191273
Wellington Delmondes <wellington.apk@gmail.com>
2023-04-28 22:08:33 -0300

    Service layer, component registration.

--- src/main/java/com/devprojects/studies/repositories/UserRepository.java
+++ src/main/java/com/devprojects/studies/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.devprojects.studies.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.devprojects.studies.entities.User;
 
+@Repository // **Optional
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 	
