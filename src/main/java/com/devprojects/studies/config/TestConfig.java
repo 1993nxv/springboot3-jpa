594d58dce97e6fd203db52463fb143fd3ce37dbd
Wellington Delmondes <wellington.apk@gmail.com>
2023-05-01 22:39:19 -0300

    OrderStatus enum.

--- src/main/java/com/devprojects/studies/config/TestConfig.java
+++ src/main/java/com/devprojects/studies/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.devprojects.studies.entities.Order;
 import com.devprojects.studies.entities.User;
+import com.devprojects.studies.entities.enums.OrderStatus;
 import com.devprojects.studies.repositories.OrderRepository;
 import com.devprojects.studies.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		
-		Order o1 = new Order(null, Instant.parse("2021-02-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2022-03-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2023-04-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2023-02-20T19:53:07Z"), OrderStatus.CANCELED, u1);
+		Order o2 = new Order(null, Instant.parse("2023-03-21T03:42:10Z"), OrderStatus.DELIVERED, u2);
+		Order o3 = new Order(null, Instant.parse("2023-04-30T15:21:22Z"), OrderStatus.SHIPPED, u1);
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
