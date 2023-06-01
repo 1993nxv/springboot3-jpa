81295c1262b402006c2e1e2b28be3dfd7e6770a2
Wellington Delmondes <wellington.apk@gmail.com>
2023-05-31 23:35:25 -0300

    Exception handling - delete.

---
+++ src/main/java/com/devprojects/studies/services/exceptions/DataBaseExcepion.java
@@ -0,0 +1,11 @@
+package com.devprojects.studies.services.exceptions;
+
+public class DataBaseExcepion extends RuntimeException {
+
+	private static final long serialVersionUID = 3593783515327716446L;
+
+	public DataBaseExcepion(String msg) {
+		super(msg);
+	}
+	
+}
